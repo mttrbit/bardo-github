8aa67f388d5bfc10ebd46edcbf479201c2872efc
Sebastian Kaiser <sk4is3r@gmail.com>
2020-04-02 14:16:01 +0200

    Add new approach of adding printers

--- ghauto-console/src/commands/issues/get.rs
+++ ghauto-console/src/commands/issues/get.rs
@@ -2,8 +2,8 @@ use client::client::{Executor, Github};
 use config::context::BardoContext;
 
 use itertools::Itertools;
+use prettytable::{format, Cell, Row, Table};
 use termion::{color, style};
-use prettytable::{Table, Row, Cell, format};
 
 #[derive(Deserialize, Debug)]
 pub struct IssueLabel {
@@ -25,6 +25,136 @@ pub struct Issue {
     assignees: Vec<IssueUser>,
     state: String,
     repository_url: String,
+}
+
+struct IssuesPrinter<'a>(&'a Vec<Issue>);
+
+trait PrintStd {
+    fn to_std_out(&self);
+}
+
+impl<'a> PrintStd for Vec<Issue> {
+    fn to_std_out(&self) {
+        IssuesPrinter(self).to_std_out();
+    }
+}
+
+impl<'a> IssuesPrinter<'a> {
+    fn print_assignees(v: &Vec<IssueUser>) -> String {
+        if (v.len() > 3) {
+            return format!(
+                "{}, ..+{}",
+                v.iter().take(3).map(|i| &i.login).join(", "),
+                v.len() - 3
+            );
+        } else {
+            return v.iter().map(|i| &i.login).join(", ");
+        }
+    }
+
+    fn extract_from_url(url: &str) -> String {
+        let mut s = String::from(url);
+        let offset = 29;
+        s.replace_range(..offset, "");
+        s
+    }
+
+    fn print_labels(v: &Vec<IssueLabel>) -> String {
+        if (v.len() > 3) {
+            return format!(
+                "{}, ... +{}",
+                v.iter().take(3).map(|i| &i.name).join(", "),
+                v.len() - 3
+            );
+        } else {
+            return v.iter().map(|i| &i.name).join(", ");
+        }
+    }
+}
+
+impl<'a> PrintStd for IssuesPrinter<'a> {
+    fn to_std_out(&self) {
+        let mut table = Table::new();
+        let format = format::FormatBuilder::new().padding(1, 1).build();
+        table.set_format(format);
+
+        table.set_titles(row![
+            format!(
+                "{}{}id{}",
+                style::Bold,
+                color::Fg(color::White),
+                style::Reset
+            ),
+            format!(
+                "{}{}org/name{}",
+                style::Bold,
+                color::Fg(color::Blue),
+                style::Reset
+            ),
+            format!(
+                "{}{}title{}",
+                style::Bold,
+                color::Fg(color::Magenta),
+                style::Reset
+            ),
+            format!(
+                "{}{}creator{}",
+                style::Bold,
+                color::Fg(color::Blue),
+                style::Reset
+            ),
+            format!(
+                "{}{}state{}",
+                style::Bold,
+                color::Fg(color::Red),
+                style::Reset
+            ),
+            format!(
+                "{}{}labels{}",
+                style::Bold,
+                color::Fg(color::White),
+                style::Reset
+            ),
+            format!(
+                "{}{}assignees{}",
+                style::Bold,
+                color::Fg(color::LightCyan),
+                style::Reset
+            ),
+        ]);
+        for e in (self.0).iter() {
+            table.add_row(row![
+                format!("{}{}{}", color::Fg(color::White), e.number, style::Reset),
+                format!(
+                    "{}{}{}",
+                    color::Fg(color::LightBlue),
+                    IssuesPrinter::extract_from_url(&e.repository_url),
+                    style::Reset
+                ),
+                format!("{}{}{}", color::Fg(color::Magenta), e.title, style::Reset),
+                format!(
+                    "{}{}{}",
+                    color::Fg(color::LightBlue),
+                    e.user.login,
+                    style::Reset
+                ),
+                format!("{}{}{}", color::Fg(color::LightRed), e.state, style::Reset),
+                format!(
+                    "{}({}){}",
+                    color::Fg(color::White),
+                    IssuesPrinter::print_labels(&e.labels),
+                    style::Reset
+                ),
+                format!(
+                    "{}{}{}",
+                    color::Fg(color::LightCyan),
+                    IssuesPrinter::print_assignees(&e.assignees),
+                    style::Reset
+                ),
+            ]);
+        }
+        table.printstd();
+    }
 }
 
 pub struct GetIssuesCommand {
@@ -50,56 +180,6 @@ impl GetIssuesCommand {
             .unwrap();
 
         let issues: Vec<Issue> = res.unwrap();
-        let mut table = Table::new();
-        let format = format::FormatBuilder::new()
-            .padding(1, 1)
-            .build();
-        table.set_format(format);
-
-        fn print_assignees(v: &Vec<IssueUser>) -> String {
-            if (v.len() > 3) {
-                return format!("{}, ... +{}", v.iter().take(3).map(|i| &i.login).join(", "), v.len()-3);
-            } else {
-               return v.iter().map(|i| &i.login).join(", ");
-            }
-        }
-
-
-        fn extract_from_url(url: &str) -> String {
-            let mut s = String::from(url);
-            let offset = 29;
-            s.replace_range(..offset, "");
-            s
-        }
-
-        fn print_labels(v: &Vec<IssueLabel>) -> String {
-            if (v.len() > 3) {
-                return format!("{}, ... +{}", v.iter().take(3).map(|i| &i.name).join(", "), v.len()-3);
-            } else {
-               return v.iter().map(|i| &i.name).join(", ");
-            }
-        }
-
-        table.set_titles(row![
-            format!("{}{}id{}", style::Bold, color::Fg(color::White), style::Reset),
-            format!("{}{}org/name{}", style::Bold, color::Fg(color::Blue), style::Reset),
-            format!("{}{}title{}", style::Bold, color::Fg(color::Magenta), style::Reset),
-            format!("{}{}creator{}", style::Bold, color::Fg(color::Blue), style::Reset),
-            format!("{}{}state{}", style::Bold, color::Fg(color::Red), style::Reset),
-            format!("{}{}labels{}", style::Bold, color::Fg(color::White), style::Reset),
-            format!("{}{}assigness{}", style::Bold, color::Fg(color::LightCyan), style::Reset),
-        ]);
-        for e in issues.iter() {
-            table.add_row(row![
-                format!("{}{}{}", color::Fg(color::White), e.number, style::Reset),
-                format!("{}{}{}", color::Fg(color::LightBlue), extract_from_url(&e.repository_url), style::Reset),
-                format!("{}{}{}", color::Fg(color::Magenta), e.title, style::Reset),
-                format!("{}{}{}", color::Fg(color::LightBlue), e.user.login, style::Reset),
-                format!("{}{}{}", color::Fg(color::LightRed), e.state, style::Reset),
-                format!("{}({}){}", color::Fg(color::White), print_labels(&e.labels), style::Reset),
-                format!("{}{}{}", color::Fg(color::LightCyan), print_assignees(&e.assignees), style::Reset),
-            ]);
-        }       
-        table.printstd();
+        issues.to_std_out();
     }
 }
